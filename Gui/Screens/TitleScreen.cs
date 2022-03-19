a03f2a02d6ca389652e88013ae3e5a86ef19e629
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-03-19 18:50:13 +0200

    Added a title screen

---
+++ Gui/Screens/TitleScreen.cs
@@ -0,0 +1,37 @@
+﻿using NuciXNA.Gui.Controls;
+using NuciXNA.Gui.Screens;
+
+namespace SokoGrump.Gui.Screens
+{
+    /// <summary>
+    /// Title screen.
+    /// </summary>
+    public class TitleScreen : MenuScreen
+    {
+        GuiMenuLink newGameLink;
+        GuiMenuItem extiAction;
+
+        /// <summary>
+        /// Loads the content.
+        /// </summary>
+        protected override void DoLoadContent()
+        {
+            newGameLink = new GuiMenuLink
+            {
+                Id = nameof(newGameLink),
+                Text = "New Game",
+                TargetScreen = typeof(GameplayScreen)
+            };
+            extiAction = new GuiMenuItem
+            {
+                Id = nameof(extiAction),
+                Text = "Exit"
+            };
+
+            Items.Add(newGameLink);
+            Items.Add(extiAction);
+
+            base.DoLoadContent();
+        }
+    }
+}
